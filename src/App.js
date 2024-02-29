ae1b8b687d60d397755283abb7ef876fce6fa266
MANASH CHOUDHURI <manashchoudhuri4@gmail.com>
2024-03-01 00:39:27 +0530

    add banner

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import AboutPage from "./components/aboutPage";
 import ContactPage from "./components/contactPage";
 import SkillsPage from "./components/skillsPage";
 import FooterPage from "./components/footerPage";
-import HomePage from "./components/homePage";
 import NavBar from "./components/navBar";
+import BannerPage from "./components/bannerPage";
+import HomePage from "./components/homePage";
 import ProjectsPage from "./components/projectsPage";
 import SocialLinks from "./components/socialLinks";
 import EducationPage from "./components/educationPage";
+
 
 function App() {
   return (
     <div>
       <NavBar />
+      <BannerPage />
       <HomePage />
       <AboutPage />
       <EducationPage />
