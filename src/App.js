7a89b56da0a6e71ed84088734f0734d3f52c4687
MANASH CHOUDHURI <manashchoudhuri4@gmail.com>
2025-04-01 12:25:10 +0530

    removed banner

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ContactPage from "./components/contactPage";
 import SkillsPage from "./components/skillsPage";
 import FooterPage from "./components/footerPage";
 import NavBar from "./components/navBar";
-import BannerPage from "./components/bannerPage";
+// import BannerPage from "./components/bannerPage";
 import HomePage from "./components/homePage";
 import ProjectsPage from "./components/projectsPage";
 import SocialLinks from "./components/socialLinks";
@@ -14,7 +14,7 @@ function App() {
   return (
     <div>
       <NavBar />
-      <BannerPage />
+      {/* <BannerPage /> */}
       <HomePage />
       <AboutPage />
       <EducationPage />
